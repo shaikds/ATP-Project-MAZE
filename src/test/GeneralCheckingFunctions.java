a74cee874af764b0535ef32ffdcbc7183c8cfaa9
shaikds <33999967+shaikds@users.noreply.github.com>
2024-06-16 17:35:52 +0300

    Update GeneralCheckingFunctions.java

--- src/test/GeneralCheckingFunctions.java
+++ src/test/GeneralCheckingFunctions.java
@@ -9,8 +9,6 @@ public class GeneralCheckingFunctions {
     }
 
     public static String getGithubLink(){
-        //change the <username> in the link to the username of the student who
-        created the github project:
         String githubLink = "https://github.com/shaikds/ATP-Project-MAZE";
         return githubLink;
     }    
